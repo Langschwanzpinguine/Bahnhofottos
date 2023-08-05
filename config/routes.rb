3835da2d7f529b0363a0c3d355735bbb47f82f00
nils-matthaei <n.matthaei@proton.me>
2023-08-05 17:14:52 +0200

    switched to latest stable ruby 3.1.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "main#index"
 
   # Defines the root path route ("/")
   # root "articles#index"
