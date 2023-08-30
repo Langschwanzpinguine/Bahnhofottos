a3e29994ae78442d0bc871b4210ace66f7a7e17d
Jakob Kohler <schasch.dev@protonmail.com>
2023-08-30 11:51:54 +0200

    Added Map route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "main#index"
 
+  get "/map", to: "map#index", as: :map
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
