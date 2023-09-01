ef018e0f1d1ef9639b645fb1af1b58ef2dca5a01
Jakob Kohler <schasch.dev@protonmail.com>
2023-09-01 18:04:46 +0200

    Updated logo and color scheme

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,4 +1,7 @@
 class ProxyController < ApplicationController
+
+  #Prallelisieren und Caching implementieren
+
   def maptiles_thunderforest
     api_key = ENV['API_KEY_THUNDERFOREST']
 
@@ -6,7 +9,7 @@ class ProxyController < ApplicationController
     y = params[:y]
     z = params[:z]
 
-    request_url = "https://tile.thunderforest.com/atlas/#{z}/#{x}/#{y}.png?apikey=#{api_key}"
+    request_url = "8https://tile.thunderforest.com/atlas/#{z}/#{x}/#{y}.png?apikey=#{api_key}"
 
     response = HTTParty.get(request_url)
 
@@ -26,16 +29,34 @@ class ProxyController < ApplicationController
     z = params[:z]
     r = params[:r]
 
+    # request_url = "https://tile.jawg.io/jawg-sunny/#{z}/#{x}/#{y}#{r}.png?access-token=#{api_key}"
+    #
+    # response = HTTParty.get(request_url)
+    #
+    # if response.success?
+    #   headers['Content-Type'] = 'image/png'
+    #   send_data response.body, disposition: 'inline'
+    # else
+    #   render plain: 'Error fetching tile', status: response.code
+    # end
+
     request_url = "https://tile.jawg.io/jawg-sunny/#{z}/#{x}/#{y}#{r}.png?access-token=#{api_key}"
+    uri = URI.parse(request_url)
 
-    response = HTTParty.get(request_url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true # Use SSL for HTTPS requests
 
-    if response.success?
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    response = http.request(request)
+
+    if response.is_a?(Net::HTTPSuccess)
       headers['Content-Type'] = 'image/png'
       send_data response.body, disposition: 'inline'
     else
-      render plain: 'Error fetching tile', status: response.code
+      render plain: 'Error fetching tile', status: response.code.to_i
     end
+
 
   end
 end
