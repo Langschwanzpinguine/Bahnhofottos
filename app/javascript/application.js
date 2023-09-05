f48544578b701ec36506c480f17df1f374f7cd8e
Jakob Kohler <schasch.dev@protonmail.com>
2023-09-05 22:06:17 +0200

    Added custom confirm modal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+Turbo.setConfirmMethod(() => {
+    let dialog = document.getElementById("turbo-confirm");
+    dialog.showModal();
+
+    return new Promise((resolve, reject) => {
+        dialog.addEventListener("close", () => {
+            resolve(dialog.returnValue === "confirm")
+        }, {once: true})
+    })
+})
