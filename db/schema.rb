81d2f47c12445d3b09d517c2c52a96c59d2e13e2
nils-matthaei <n.matthaei@proton.me>
2023-09-20 09:49:48 +0200

    Create TrainStation model with OSM ID and user association
    Generated a new TrainStation model with attributes for OpenStreetMap ID and a user association. Created a migration that establishes a unique constraint on the combination of osm_id and user_id to ensure each user can have only one train station with a specific OSM ID. The model also includes Active Storage for attaching images to train stations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_15_095148) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_073639) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,6 +48,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_15_095148) do
     t.index ["user_id"], name: "index_invitations_on_user_id"
   end
 
+  create_table "train_stations", force: :cascade do |t|
+    t.string "osm_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["osm_id", "user_id"], name: "index_train_stations_on_osm_id_and_user_id", unique: true
+    t.index ["user_id"], name: "index_train_stations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
@@ -59,4 +68,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_15_095148) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "invitations", "users"
+  add_foreign_key "train_stations", "users"
 end
