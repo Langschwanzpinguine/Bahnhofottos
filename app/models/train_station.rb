586050a01ef919d1d374054845ca6e3ddf77cd6d
nils-matthaei <n.matthaei@proton.me>
2023-09-20 09:58:00 +0200

    TrainStation model now validates presence of image

--- app/models/train_station.rb
+++ app/models/train_station.rb
@@ -2,4 +2,5 @@ class TrainStation < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   validates :osm_id, presence: true, uniqueness: { scope: :user_id, case_sensitive: false }
+  validates :image, presence: true
 end
