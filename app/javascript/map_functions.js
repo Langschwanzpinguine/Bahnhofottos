3ebd633feff60860d7d0cd2c7f836212e6a7f08b
Jakob Kohler <schasch.dev@protonmail.com>
2023-09-20 22:00:00 +0200

    Buggy search

--- app/javascript/map_functions.js
+++ app/javascript/map_functions.js
@@ -2,12 +2,19 @@ let map;
 let markers;
 let selectedCountry;
 let select_tag;
+let listOfResults;
+let searchInput;
 
 const STATION_GREEN = "/assets/bahnhof_windowless_green";
 const STATION_YELLOW = "/assets/bahnhof_windowless_yellow";
 const STATION_RED = "/assets/bahnhof_windowless_red";
 
 document.addEventListener("DOMContentLoaded", initPage);
+let fuse = new Fuse([], {
+    keys: [
+        "tags.name"
+    ]}
+);
 
 
 function countrySelected(){
@@ -50,11 +57,20 @@ function countrySelected(){
             return res.json();
         }).then(data => {
         let allStations = data.elements;
+
+        const fuseOptions = {
+            keys: [
+                "tags.name"
+            ]
+        };
+        fuse = new Fuse(allStations, fuseOptions);
+
         for (const station of allStations) {
             let lat = station.lat;
             let lon = station.lon;
             let marker = L.marker([lat, lon]);
-            marker.bindPopup(station.tags.name ?? station.tags.description);
+
+            marker.bindPopup(createPopUp(station));
             markers.addLayer(marker);
             map.addLayer(markers);
         }
@@ -64,10 +80,26 @@ function countrySelected(){
 
 }
 
+function createPopUp(station){
+    let operator_string = station.tags.operator
+    let returnString = `<h5>${station.tags.name ?? station.tags.description}</h5>
+    <p><i class="fa-solid fa-train"></i> ${station.tags.railway.charAt(0).toUpperCase() + station.tags.railway.slice(1)}</p>`
+    if(operator_string){
+        let op_arr = operator_string.split(';')
+        for(const operator of op_arr){
+            returnString += `<p><i class="fa-solid fa-building"></i> ${operator}</p>`
+        }
+
+    }
+    return returnString;
+}
+
 function initPage(){
-    select_tag = document.getElementById('country_selection')
+    searchInput = document.getElementById('station_search');
+    select_tag = document.getElementById('country_selection');
     selectedCountry = select_tag.value;
-    select_tag.addEventListener('change', countrySelected)
+    select_tag.addEventListener('change', countrySelected);
+    listOfResults = document.getElementById('matches');
     initMap();
     countrySelected();
     const element = document.querySelector('.country_dropdown');
@@ -77,6 +109,11 @@ function initPage(){
         searchPlaceholderValue: "Search...",
         shouldSort: false
     });
+
+    document.addEventListener('click', (ev) => {
+        disableResults(ev);
+    });
+    document.getElementById('station_search').addEventListener('input', perform_search);
 }
 
 function initMap(){
@@ -99,48 +136,41 @@ function initMap(){
     // map.attributionControl.addAttribution("OSM x <a href='https://github.com/Langschwanzpinguine' target='_blank'>Langschwanzpinguine e.V.</a>")
 }
 
+function perform_search(){
+    let pattern = document.getElementById('station_search').value;
+    let results = fuse.search(pattern).slice(0, 5);
+    listOfResults.innerHTML = '';
+    listOfResults.style.display = 'block';
 
-function fetchTrainStationsInView(){
-    markersLayer.clearLayers();
-    const bounding_box = map.getBounds();
-    const request_body = {
-        start_lat: bounding_box["_northEast"]["lat"],
-        start_lon: bounding_box["_northEast"]["lng"],
-        end_lat: bounding_box["_southWest"]["lat"],
-        end_lon: bounding_box["_southWest"]["lng"],
+    for (const res of results) {
+        const li = document.createElement('li');
+        li.setAttribute('data-id', res.item.id);
+        li.textContent = res.item.tags.name;
+
+        li.addEventListener('click', (ev) => {
+            viewSearchedStation(ev);
+        });
+
+        listOfResults.appendChild(li);
     }
-
-    const url = "http://localhost:3000/api/overpass/stations";
-    const requestOptions = {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(request_body),
-    };
-    console.log(request_body);
-
-    fetch(url, requestOptions)
-        .then(res => {
-            if (!res.ok) {
-                console.error(`HTTP error! Status: ${res.status}`);
-                return;
-            }
-            console.log(res)
-            return res.json();
-        }).then(data => {
-            console.log('Response data:', data);
-            let allStations = data.elements;
-            for (const station of allStations) {
-                let lat = station.lat;
-                let lon = station.lon;
-                let marker = L.marker([lat, lon]).addTo(map);
-                marker.bindPopup(station.tags.name ?? station.tags.description);
-                markersLayer.addLayer(marker);
-        }
-    }).catch(error => {
-        console.error('Fetch error:', error);
-    });
 }
+
+function disableResults(event){
+    if (event.target !== searchInput && event.target !== listOfResults && !listOfResults.contains(event.target)) {
+        const liElements = document.querySelectorAll('.matches li');
+        liElements.forEach((li) => {
+            console.log("Mounted listener")
+            li.removeEventListener('click', viewSearchedStation);
+        });
+        document.getElementById('station_search').value = "";
+        listOfResults.style.display = 'none';
+    }
+}
+
+function viewSearchedStation(event){
+    console.log(`Clicked on ${event.target.getAttribute('data-id')}`)
+}
+
+
+
 
