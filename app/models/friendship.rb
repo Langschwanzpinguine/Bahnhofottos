1ab0255edb7c46b1e71eb89ef73df6c9367ba6b6
Jakob Kohler <schasch.dev@protonmail.com>
2024-08-03 15:10:38 +0200

    Remove redundant clause

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,8 +3,7 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   def self.unfriend(user_id, friend_id)
-    all_friendships = FriendshipsQuery.both_ways(user_id: user_id)
-    friendship = all_friendships.where(friend_id: friend_id).or(all_friendships.where(friend_id: user_id)).first
+    friendship = FriendshipsQuery.both_ways(user_id: user_id, friend_id: friend_id).first
     friendship.destroy if friendship
   end
 end
